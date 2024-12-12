c1035fba2fee18cb4fd8421db985a239010d6f45
JahongirOmonov <dodajoy077@mail.ru>
2024-12-12 10:43:28 +0500

    feat: admin_latin

--- src/urls.py
+++ src/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin_latin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
